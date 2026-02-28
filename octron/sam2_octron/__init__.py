64824da0713f3c4958724e7c580e70173145566c
Hotte <hobenhaus@gmail.com>
2026-02-28 11:08:47 -0500

    Get rid of bloated __init__ code

--- octron/sam2_octron/__init__.py
+++
@@ -1,2 +0,0 @@
-from .helpers.sam3_octron import SAM3_octron, SAM3_semantic_octron, run_new_pred as run_new_pred_sam3
-from .helpers.build_sam3_octron import build_sam3_octron
--- octron/sam2_octron/helpers/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import sam2_checks
-from . import sam2_octron
-from . import sam2hq_octron
-from . import build_sam2_octron
-
-from . import sam2_colors
-from . import sam2_zarr
-from . import sam2_layer
