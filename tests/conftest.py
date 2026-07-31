d409ee8b1621fbf1c8d97c8ba7f52bf4946fdbb5
sfmig <33267254+sfmig@users.noreply.github.com>
2026-07-31 00:13:15 -0700

    Drop fixtures module for now

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,24 +3,12 @@
 Follows movement's structure at https://github.com/neuroinformatics-unit/movement/tree/main/tests
 """
 
-from pathlib import Path
-
 import numpy as np
 import pytest
 
-# define `fixtures`` module path
-# by searching relative to this file
-# (we assume the structure is always
-#  tests.fixtures.<module_name>)
-fixtures_dir = Path(__file__).parent / "fixtures"
-pytest_plugins = [
-    f"tests.fixtures.{fixture.stem}"
-    for fixture in fixtures_dir.glob("*.py")
-    if "__" not in fixture.name
-]
-
 
 # define other fixtures shared by the whole suite here
+# Example:
 @pytest.fixture(scope="session")
 def rng():
     """Return a random number generator with a fixed seed."""
